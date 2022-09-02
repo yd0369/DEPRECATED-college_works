350aa81e6d4c596dc213ed09d35c6ffabfdec238
Yash Desai (yd0369) <yd0369>
2022-09-02 18:40:22 +0530

    Monday, July 30, 2018 12:47:02 AM IST

--- Practicals/HSD - 22316 - Object Oriented Programming using C++ (OOP)/Practical 1/1 Quadratic Equation.cpp
+++ Practicals/HSD - 22316 - Object Oriented Programming using C++ (OOP)/Practical 1/1 Quadratic Equation.cpp
@@ -1,25 +1,25 @@
 ////////////////////////////////////////////
 //  O.O.P. PRACTICAL 1 -[ Question 1 ]    //
-//     ----Quadratic Equation----         // 
+//     ----Quadratic Equation----         //
 //                                        //
 //              YASH DESAI                //
 ////////////////////////////////////////////
 
 #include<iostream>
+
 using namespace std;
-main()
-{
-	int A,B,C;
-	cout<<"Enter the value of A : ";
-	cin>>A;
-	cout<<"Enter the value of B : ";
-	cin>>B;
-	cout<<"Enter the value of C : ";
-	cin>>C;
-	cout<<endl<<endl<<endl<<endl;
-	cout<<"The roots of the following Quadratic Equqation are :";
-	cout<<endl<<"Root 1 :";
-	cout<<((-B+((B*B)-((4*A*C)^(1/2))))/(2*A));
-	cout<<endl<<"Root 2 :";
-	cout<<((-B-((B*B)-((4*A*C)^(1/2))))/(2*A));
+main() {
+    int A, B, C;
+    cout << "Enter the value of A : ";
+    cin >> A;
+    cout << "Enter the value of B : ";
+    cin >> B;
+    cout << "Enter the value of C : ";
+    cin >> C;
+    cout << endl << endl << endl << endl;
+    cout << "The roots of the following Quadratic Equqation are :";
+    cout << endl << "Root 1 :";
+    cout << ((-B + ((B * B) - ((4 * A * C) ^ (1 / 2)))) / (2 * A));
+    cout << endl << "Root 2 :";
+    cout << ((-B - ((B * B) - ((4 * A * C) ^ (1 / 2)))) / (2 * A));
 }
