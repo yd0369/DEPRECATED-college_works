cc5c635f9b5b3753532fcdf8c57cc183810accf7
Yash Desai (yd0369) <yd0369>
2022-09-02 18:40:31 +0530

    Monday, July 30, 2018 12:47:02 AM IST

--- Practicals/HSD - 22316 - Object Oriented Programming using C++ (OOP)/Practical 1/2 Operator Precdence.cpp
+++ Practicals/HSD - 22316 - Object Oriented Programming using C++ (OOP)/Practical 1/2 Operator Precdence.cpp
@@ -6,30 +6,28 @@
 ////////////////////////////////////////////
 
 #include<iostream>
+
 using namespace std;
-main()
-{
-	int A,B,C,D;
-	cout<<"Enter the value of A : ";
-	cin>>A;
-	cout<<"Enter the value of B : ";
-	cin>>B;
-	cout<<"Enter the value of C : ";
-	cin>>C;
-	cout<<"Enter the value of D : ";
-	cin>>D;
-	cout<<endl<<endl<<endl;
-	cout<<"(    A *     B   +   C     * D    ) : "<<(A*B+C*D)<<endl;
-	cout<<"( (  A *     B ) +   C     * D    ) : "<<((A*B)+C*D)<<endl;
-	cout<<"(    A *  (  B   +   C )   * D    ) : "<<(A*(B+C)*D)<<endl;
-	cout<<"(    A *     B   + ( C     * D  ) ) : "<<(A*B+(C*D))<<endl;
-	cout<<"( (  A *     B ) + ( C     * D  ) ) : "<<((A*B)+(C*D))<<endl;
-	cout<<"( (( A *     B ) +   C   ) * D    ) : "<<(((A*B)+C)*D)<<endl;
-	cout<<"( (  A *  (  B   +   C ) ) * D    ) : "<<((A*(B+C))*D)<<endl;
-	cout<<"( (  A *     B   +   C   ) * D    ) : "<<((A*B+C)*D)<<endl;
-	cout<<"(    A *  (( B   +   C )   * D  ) ) : "<<(A*((B+C)*D))<<endl;
-	cout<<"(    A *  (  B   + ( C     * D )) ) : "<<(A*(B+(C*D)))<<endl;
-	cout<<"(    A *  (  B   +   C     * D  ) ) : "<<(A*(B+C*D))<<endl;
+main() {
+    int A, B, C, D;
+    cout << "Enter the value of A : ";
+    cin >> A;
+    cout << "Enter the value of B : ";
+    cin >> B;
+    cout << "Enter the value of C : ";
+    cin >> C;
+    cout << "Enter the value of D : ";
+    cin >> D;
+    cout << endl << endl << endl;
+    cout << "(    A *     B   +   C     * D    ) : " << (A * B + C * D) << endl;
+    cout << "( (  A *     B ) +   C     * D    ) : " << ((A * B) + C * D) << endl;
+    cout << "(    A *  (  B   +   C )   * D    ) : " << (A * (B + C) * D) << endl;
+    cout << "(    A *     B   + ( C     * D  ) ) : " << (A * B + (C * D)) << endl;
+    cout << "( (  A *     B ) + ( C     * D  ) ) : " << ((A * B) + (C * D)) << endl;
+    cout << "( (( A *     B ) +   C   ) * D    ) : " << (((A * B) + C) * D) << endl;
+    cout << "( (  A *  (  B   +   C ) ) * D    ) : " << ((A * (B + C)) * D) << endl;
+    cout << "( (  A *     B   +   C   ) * D    ) : " << ((A * B + C) * D) << endl;
+    cout << "(    A *  (( B   +   C )   * D  ) ) : " << (A * ((B + C) * D)) << endl;
+    cout << "(    A *  (  B   + ( C     * D )) ) : " << (A * (B + (C * D))) << endl;
+    cout << "(    A *  (  B   +   C     * D  ) ) : " << (A * (B + C * D)) << endl;
 }
-
-
