8eb423709d1c7e3c9f03c0ea8676844c9eb291d8
Yash Desai (yd0369) <yd0369>
2022-09-02 18:20:53 +0530

    Monday, July 30, 2018 12:47:02 AM IST

---
+++ Practicals/HSD - 22316 - Object Oriented Programming using C++ (OOP)/Practical 10/1 Addition , Subtraction, Multiplication and Division  using constuctor and Destructor.cpp
@@ -0,0 +1,55 @@
+////////////////////////////////////////////
+//  O.O.P. PRACTICAL 10 -[ Question 1 ]   //
+//-- Constructor And Destructor Function--// 
+//                                        //
+//              YASH DESAI                //
+////////////////////////////////////////////
+
+#include<iostream>
+using namespace std;
+class Number
+{
+	private:
+		double X,Y;
+		
+	public:	
+		Number()
+		{
+			get_data();
+			cout << "ADDITION : "<<ADDITION() <<endl;
+			cout << "SUBTRACTION : "<<SUBTRACTION() <<endl;
+			cout << "MULTIPLICATION : "<<MULTIPLICATION() <<endl;
+			cout << "DIVISION : "<<DIVISION() <<endl;		
+		}
+
+		~Number()
+		{
+			cout<<endl<<"Destructor Intialized...."<<endl;
+		}
+
+	    void get_data()
+		{
+			cout<<"Enter the value of First Number : ";
+		 	cin>>X;
+			cout<<"Enter the value of Second Number : ";
+			cin>>Y;
+		}
+		double ADDITION()
+		{
+			return (X+Y);
+		}	
+		double SUBTRACTION()
+		{
+			return (X-Y);
+		}	
+		double MULTIPLICATION()
+		{
+			return (X*Y);
+		}	
+		double DIVISION()
+		{
+			return (X/Y);
+		}	
+			
+}N;
+main(){}
