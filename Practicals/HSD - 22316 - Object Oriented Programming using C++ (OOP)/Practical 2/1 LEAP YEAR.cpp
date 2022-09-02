75bb371bdd72b1bb504959f7287987214d24ac8e
Yash Desai (yd0369) <yd0369>
2022-09-02 18:40:37 +0530

    Monday, July 30, 2018 12:47:02 AM IST

--- Practicals/HSD - 22316 - Object Oriented Programming using C++ (OOP)/Practical 2/1 LEAP YEAR.cpp
+++ Practicals/HSD - 22316 - Object Oriented Programming using C++ (OOP)/Practical 2/1 LEAP YEAR.cpp
@@ -6,19 +6,16 @@
 ////////////////////////////////////////////
 
 #include<iostream>
+
 using namespace std;
-main()
-{
-	int Year;
-	cout<<"Enter the Year : ";
-	cin>>Year;
-	cout<<endl<<endl<<endl;
-	if(( Year % 100 != 0 && Year % 4 == 0) || Year % 400 == 0 )
-	{
-		cout<<Year<<" is a Leap Year.";
-	}
-	else
-	{
-		cout<<Year<<" is not Leap Year.";
-	}
+main() {
+    int Year;
+    cout << "Enter the Year : ";
+    cin >> Year;
+    cout << endl << endl << endl;
+    if ((Year % 100 != 0 && Year % 4 == 0) || Year % 400 == 0) {
+        cout << Year << " is a Leap Year.";
+    } else {
+        cout << Year << " is not Leap Year.";
+    }
 }
