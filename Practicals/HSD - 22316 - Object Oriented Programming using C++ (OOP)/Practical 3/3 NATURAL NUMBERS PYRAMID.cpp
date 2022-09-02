4e98c336da0d2b7d29c7fc136b83a72fb0d10a62
Yash Desai (yd0369) <yd0369>
2022-09-02 18:40:53 +0530

    Monday, July 30, 2018 12:47:02 AM IST

--- Practicals/HSD - 22316 - Object Oriented Programming using C++ (OOP)/Practical 3/3 NATURAL NUMBERS PYRAMID.cpp
+++ Practicals/HSD - 22316 - Object Oriented Programming using C++ (OOP)/Practical 3/3 NATURAL NUMBERS PYRAMID.cpp
@@ -6,18 +6,16 @@
 ////////////////////////////////////////////
 
 #include<iostream>
+
 using namespace std;
-main()
-{
-	int NATURAL_NUM=1,LOOP=1,LOOP_2;
-	while(LOOP<=5)
-	{
-  		for (LOOP_2=0;LOOP_2<LOOP;LOOP_2++)
-    	{
-		    cout<<"\t"<<NATURAL_NUM;
-         	NATURAL_NUM++;
+main() {
+    int NATURAL_NUM = 1, LOOP = 1, LOOP_2;
+    while (LOOP <= 5) {
+        for (LOOP_2 = 0; LOOP_2 < LOOP; LOOP_2++) {
+            cout << "\t" << NATURAL_NUM;
+            NATURAL_NUM++;
         }
-    LOOP++;
-    cout<<endl;
+        LOOP++;
+        cout << endl;
     }
 }
