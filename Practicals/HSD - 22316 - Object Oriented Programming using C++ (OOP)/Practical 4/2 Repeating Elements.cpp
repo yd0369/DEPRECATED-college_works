b6d014e9045b42918029175b83798fc1ff878072
Yash Desai (yd0369) <yd0369>
2022-09-02 18:41:04 +0530

    Monday, July 30, 2018 12:47:02 AM IST

--- Practicals/HSD - 22316 - Object Oriented Programming using C++ (OOP)/Practical 4/2 Repeating Elements.cpp
+++ Practicals/HSD - 22316 - Object Oriented Programming using C++ (OOP)/Practical 4/2 Repeating Elements.cpp
@@ -6,33 +6,28 @@
 ////////////////////////////////////////////
 
 #include<iostream>
+
 using namespace std;
-main()
-{
-	int ARRAY[100],SIZE,LOOP_1,LOOP_2,TEMP,TIMES;
-	cout<<"Enter the Size of Array [0-99] : ";
-	cin>>SIZE;
-	cout<<endl<<endl<<endl;
-	cout<<"Enter the elements of Array:"<<endl;
-	for(LOOP_1=0;LOOP_1<SIZE;LOOP_1++)	
-	{
-		cin>>ARRAY[LOOP_1];
-	}
-	for(LOOP_1=0;LOOP_1<SIZE;LOOP_1++)
-	{
-		TEMP=ARRAY[LOOP_1];
-		TIMES=0;
-		for(LOOP_2=0;LOOP_2<SIZE;LOOP_2++)
-		{
-			if(TEMP==ARRAY[LOOP_2])
-			{
-				TIMES++;
-			}
-		}
-		if(TIMES!=0 && TIMES!=1)
-		{
-			cout<<"The "<<LOOP_1<<" element with value "<<ARRAY[LOOP_1];
-			cout<<" is repeated "<<TIMES<<endl; 
-		}
-	}
+main() {
+    int ARRAY[100], SIZE, LOOP_1, LOOP_2, TEMP, TIMES;
+    cout << "Enter the Size of Array [0-99] : ";
+    cin >> SIZE;
+    cout << endl << endl << endl;
+    cout << "Enter the elements of Array:" << endl;
+    for (LOOP_1 = 0; LOOP_1 < SIZE; LOOP_1++) {
+        cin >> ARRAY[LOOP_1];
+    }
+    for (LOOP_1 = 0; LOOP_1 < SIZE; LOOP_1++) {
+        TEMP = ARRAY[LOOP_1];
+        TIMES = 0;
+        for (LOOP_2 = 0; LOOP_2 < SIZE; LOOP_2++) {
+            if (TEMP == ARRAY[LOOP_2]) {
+                TIMES++;
+            }
+        }
+        if (TIMES != 0 && TIMES != 1) {
+            cout << "The " << LOOP_1 << " element with value " << ARRAY[LOOP_1];
+            cout << " is repeated " << TIMES << endl;
+        }
+    }
 }
