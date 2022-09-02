d515c65ca360b18993e2db137063c67638907a6f
Yash Desai (yd0369) <yd0369>
2022-09-02 18:21:29 +0530

    Monday, July 30, 2018 12:47:02 AM IST

---
+++ Practicals/HSD - 22316 - Object Oriented Programming using C++ (OOP)/Practical 14/1 Class Book accessing with Pointer.cpp
@@ -0,0 +1,32 @@
+////////////////////////////////////////////
+//  O.O.P. PRACTICAL 14 -[ Question 1 ]   //
+//  -- Classes ,Objects and Pointer --    // 
+//                                        //
+//              YASH DESAI                //
+////////////////////////////////////////////
+
+#include<iostream>
+using namespace std;
+class Book
+{
+	private:
+		char book_name[10],auther_name[10];
+		double price;
+		
+	public:	
+		void get()
+		{
+			cout<<"Enter the Book Name : ";
+		 	cin>>book_name;
+			cout<<"Enter the auther_name : ";
+			cin>>auther_name;
+			cout<<"Enter the Book Price : ";
+			cin>>price;
+		}	
+};
+main()
+{
+	Book B1,*B2;
+	B2=&B1;
+	B2->get();	
+}
