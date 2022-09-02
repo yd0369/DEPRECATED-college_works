71a693855f3f4b555c91e5a3cf3ee9579de4c12a
Yash Desai (yd0369) <yd0369>
2022-09-02 18:41:17 +0530

    Monday, July 30, 2018 12:47:02 AM IST

--- Practicals/HSD - 22316 - Object Oriented Programming using C++ (OOP)/Practical 5/1 3BY3 MATRIX MULTIPLIER ####.cpp
+++ Practicals/HSD - 22316 - Object Oriented Programming using C++ (OOP)/Practical 5/1 3BY3 MATRIX MULTIPLIER ####.cpp
@@ -6,89 +6,70 @@
 ////////////////////////////////////////////
 
 #include<iostream>
+
 using namespace std;
-main()
-{
-	int MATRIX_1[3][3][1],MATRIX_2[3][3][1],MATRIX_PRODUCT[3][3][1];
-	int LOOP_1,LOOP_2,LOOP_3,LOGIC_LOOP,TEMP;
-	cout<<"Enter the Values of First Matrix :"<<endl;
-	for(LOOP_1=0;LOOP_1<3;LOOP_1++)
-	{
-		for(LOOP_2=0;LOOP_2<3;LOOP_2++)
-	    {
-	    	for(LOOP_3=0;LOOP_3<1;LOOP_3++)
-	    	{
-	    		cin>>MATRIX_1[LOOP_1][LOOP_2][LOOP_3];
-    		}
-    	}
-	}
- 	cout<<"Enter the Values of Second Matrix :"<<endl;
- 	for(LOOP_1=0;LOOP_1<3;LOOP_1++)
-	{
-		for(LOOP_2=0;LOOP_2<3;LOOP_2++)
-	    {
-	    	for(LOOP_3=0;LOOP_3<1;LOOP_3++)
-	    	{
-	    		cin>>MATRIX_2[LOOP_1][LOOP_2][LOOP_3];
-   			}
-   		}
-	}
-	cout<<endl<<endl<<endl;	
-	cout<<"Product of two Matrix :"<<endl;
-	cout<<"Matrix A :"<<endl<<endl;
-	for(LOOP_1=0;LOOP_1<3;LOOP_1++)
-	{
-		for(LOOP_2=0;LOOP_2<3;LOOP_2++)
-	    {
-	    	for(LOOP_3=0;LOOP_3<1;LOOP_3++)
-	    	{
-	    		cout<<MATRIX_1[LOOP_1][LOOP_2][LOOP_3];
-    		}
-			cout<<"\t";	
-    	}
-		cout<<endl;	
-	}
-	cout<<endl;	
-	cout<<"Matrix B :"<<endl<<endl;
-	for(LOOP_1=0;LOOP_1<3;LOOP_1++)
-	{
-		for(LOOP_2=0;LOOP_2<3;LOOP_2++)
-	    {
-	    	for(LOOP_3=0;LOOP_3<1;LOOP_3++)
-	    	{
-	    		cout<<MATRIX_2[LOOP_1][LOOP_2][LOOP_3];
-    		}
-			cout<<"\t";	
-    	}
-		cout<<endl;	
-	}
-	for(LOOP_1=0;LOOP_1<3;LOOP_1++)
-	{
-		for(LOOP_2=0;LOOP_2<3;LOOP_2++)
-	    {
-	    	for(LOOP_3=0;LOOP_3<1;LOOP_3++)
-		    {
-		    	MATRIX_PRODUCT[LOOP_1][LOOP_2][LOOP_3]=0;
-	    		for(LOGIC_LOOP=1;LOGIC_LOOP<3;LOGIC_LOOP++)
-	    		{
-	    			MATRIX_PRODUCT[LOOP_1][LOOP_2][LOOP_3]+=(MATRIX_1[LOOP_1][LOOP_2][LOOP_3]*MATRIX_2[LOOP_1][LOOP_2][LOOP_3]);
-    			}
-    	    }	
-    	}
-	}
-	cout<<endl;	
-	cout<<"Matrix Product :"<<endl<<endl;
-	for(LOOP_1=0;LOOP_1<3;LOOP_1++)
-	{
-		for(LOOP_2=0;LOOP_2<3;LOOP_2++)
-	    {
-	    	for(LOOP_3=0;LOOP_3<1;LOOP_3++)
-	    	{
-	    		cout<<MATRIX_PRODUCT[LOOP_1][LOOP_2][LOOP_3];
-    		}
-			cout<<"\t";	
-    	}
-		cout<<endl;	
-	}
-	
+main() {
+    int MATRIX_1[3][3][1], MATRIX_2[3][3][1], MATRIX_PRODUCT[3][3][1];
+    int LOOP_1, LOOP_2, LOOP_3, LOGIC_LOOP, TEMP;
+    cout << "Enter the Values of First Matrix :" << endl;
+    for (LOOP_1 = 0; LOOP_1 < 3; LOOP_1++) {
+        for (LOOP_2 = 0; LOOP_2 < 3; LOOP_2++) {
+            for (LOOP_3 = 0; LOOP_3 < 1; LOOP_3++) {
+                cin >> MATRIX_1[LOOP_1][LOOP_2][LOOP_3];
+            }
+        }
+    }
+    cout << "Enter the Values of Second Matrix :" << endl;
+    for (LOOP_1 = 0; LOOP_1 < 3; LOOP_1++) {
+        for (LOOP_2 = 0; LOOP_2 < 3; LOOP_2++) {
+            for (LOOP_3 = 0; LOOP_3 < 1; LOOP_3++) {
+                cin >> MATRIX_2[LOOP_1][LOOP_2][LOOP_3];
+            }
+        }
+    }
+    cout << endl << endl << endl;
+    cout << "Product of two Matrix :" << endl;
+    cout << "Matrix A :" << endl << endl;
+    for (LOOP_1 = 0; LOOP_1 < 3; LOOP_1++) {
+        for (LOOP_2 = 0; LOOP_2 < 3; LOOP_2++) {
+            for (LOOP_3 = 0; LOOP_3 < 1; LOOP_3++) {
+                cout << MATRIX_1[LOOP_1][LOOP_2][LOOP_3];
+            }
+            cout << "\t";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    cout << "Matrix B :" << endl << endl;
+    for (LOOP_1 = 0; LOOP_1 < 3; LOOP_1++) {
+        for (LOOP_2 = 0; LOOP_2 < 3; LOOP_2++) {
+            for (LOOP_3 = 0; LOOP_3 < 1; LOOP_3++) {
+                cout << MATRIX_2[LOOP_1][LOOP_2][LOOP_3];
+            }
+            cout << "\t";
+        }
+        cout << endl;
+    }
+    for (LOOP_1 = 0; LOOP_1 < 3; LOOP_1++) {
+        for (LOOP_2 = 0; LOOP_2 < 3; LOOP_2++) {
+            for (LOOP_3 = 0; LOOP_3 < 1; LOOP_3++) {
+                MATRIX_PRODUCT[LOOP_1][LOOP_2][LOOP_3] = 0;
+                for (LOGIC_LOOP = 1; LOGIC_LOOP < 3; LOGIC_LOOP++) {
+                    MATRIX_PRODUCT[LOOP_1][LOOP_2][LOOP_3] += (MATRIX_1[LOOP_1][LOOP_2][LOOP_3] * MATRIX_2[LOOP_1][LOOP_2][LOOP_3]);
+                }
+            }
+        }
+    }
+    cout << endl;
+    cout << "Matrix Product :" << endl << endl;
+    for (LOOP_1 = 0; LOOP_1 < 3; LOOP_1++) {
+        for (LOOP_2 = 0; LOOP_2 < 3; LOOP_2++) {
+            for (LOOP_3 = 0; LOOP_3 < 1; LOOP_3++) {
+                cout << MATRIX_PRODUCT[LOOP_1][LOOP_2][LOOP_3];
+            }
+            cout << "\t";
+        }
+        cout << endl;
+    }
+
 }
