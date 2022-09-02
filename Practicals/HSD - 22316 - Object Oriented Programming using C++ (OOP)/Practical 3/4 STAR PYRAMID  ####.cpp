2d5e954e5391d088e2af59570e696626699cdc73
Yash Desai (yd0369) <yd0369>
2022-09-02 18:40:56 +0530

    Monday, July 30, 2018 12:47:02 AM IST

--- Practicals/HSD - 22316 - Object Oriented Programming using C++ (OOP)/Practical 3/4 STAR PYRAMID  ####.cpp
+++ Practicals/HSD - 22316 - Object Oriented Programming using C++ (OOP)/Practical 3/4 STAR PYRAMID  ####.cpp
@@ -6,24 +6,21 @@
 ////////////////////////////////////////////
 
 #include<iostream>
+
 using namespace std;
-main()
-{
-	int LOOP=1,LOOP_2,LOOP_3,LOOP_4,SIZE;
-	cout<<"Enter the Number of Rows :";
-	cin>>SIZE;
-	while(LOOP<=SIZE)
-	{
-		for(LOOP_2=1;LOOP_2<LOOP_4;LOOP_2++)
-		{
-			cout<<"\t";
-		}
-		for(LOOP_3=1;LOOP_3<LOOP_2;LOOP_3++)
-		{
-			cout<<"*";
-		}
-	LOOP_4--;
-    LOOP++;
-    cout<<endl;
+main() {
+    int LOOP = 1, LOOP_2, LOOP_3, LOOP_4, SIZE;
+    cout << "Enter the Number of Rows :";
+    cin >> SIZE;
+    while (LOOP <= SIZE) {
+        for (LOOP_2 = 1; LOOP_2 < LOOP_4; LOOP_2++) {
+            cout << "\t";
+        }
+        for (LOOP_3 = 1; LOOP_3 < LOOP_2; LOOP_3++) {
+            cout << "*";
+        }
+        LOOP_4--;
+        LOOP++;
+        cout << endl;
     }
 }
