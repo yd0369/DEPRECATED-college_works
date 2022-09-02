dec3930515e10a0f86b511bb40a6eebbc5ffc413
Yash Desai (yd0369) <yd0369>
2022-09-02 18:41:08 +0530

    Monday, July 30, 2018 12:47:02 AM IST

--- Practicals/HSD - 22316 - Object Oriented Programming using C++ (OOP)/Practical 4/3 Smallest and Second Smallest Element Finder.cpp
+++ Practicals/HSD - 22316 - Object Oriented Programming using C++ (OOP)/Practical 4/3 Smallest and Second Smallest Element Finder.cpp
@@ -6,34 +6,29 @@
 ////////////////////////////////////////////
 
 #include<iostream>
+
 using namespace std;
-main()
-{
-	int ARRAY[100],SIZE,LOOP,SMALL,SMALLJR;
-	cout<<"Enter the Size of Array [0-99] : ";
-	cin>>SIZE;
-	cout<<endl<<endl<<endl;
-	cout<<"Enter the elements of Array:"<<endl;
-	for(LOOP=0;LOOP<SIZE;LOOP++)	
-	{
-		cin>>ARRAY[LOOP];
-	}
-	SMALL=SMALLJR=9999;
-	for(LOOP=0;LOOP<SIZE;LOOP++)
-	{
-		if(ARRAY[LOOP]<SMALL)
-		{
-			SMALL=ARRAY[LOOP];
-		}
-	}
-	for(LOOP=0;LOOP<SIZE;LOOP++)
-	{
-		if(ARRAY[LOOP]<SMALLJR && ARRAY[LOOP]!=SMALL)
-		{
-			SMALLJR=ARRAY[LOOP];
-		}
-	}
-	cout<<endl<<endl<<endl;
-	cout<<"SMALLEST NUMBER : "<<SMALL<<endl;
-	cout<<"SECOND SMALLEST NUMBER : "<<SMALLJR;
+main() {
+    int ARRAY[100], SIZE, LOOP, SMALL, SMALLJR;
+    cout << "Enter the Size of Array [0-99] : ";
+    cin >> SIZE;
+    cout << endl << endl << endl;
+    cout << "Enter the elements of Array:" << endl;
+    for (LOOP = 0; LOOP < SIZE; LOOP++) {
+        cin >> ARRAY[LOOP];
+    }
+    SMALL = SMALLJR = 9999;
+    for (LOOP = 0; LOOP < SIZE; LOOP++) {
+        if (ARRAY[LOOP] < SMALL) {
+            SMALL = ARRAY[LOOP];
+        }
+    }
+    for (LOOP = 0; LOOP < SIZE; LOOP++) {
+        if (ARRAY[LOOP] < SMALLJR && ARRAY[LOOP] != SMALL) {
+            SMALLJR = ARRAY[LOOP];
+        }
+    }
+    cout << endl << endl << endl;
+    cout << "SMALLEST NUMBER : " << SMALL << endl;
+    cout << "SECOND SMALLEST NUMBER : " << SMALLJR;
 }
