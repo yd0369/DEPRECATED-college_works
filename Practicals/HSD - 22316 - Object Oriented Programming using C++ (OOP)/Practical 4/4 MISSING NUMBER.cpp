c4110ef6c5328b7ac77c07a5f8b4d86530c5a76c
Yash Desai (yd0369) <yd0369>
2022-09-02 18:41:13 +0530

    Monday, July 30, 2018 12:47:02 AM IST

--- Practicals/HSD - 22316 - Object Oriented Programming using C++ (OOP)/Practical 4/4 MISSING NUMBER.cpp
+++ Practicals/HSD - 22316 - Object Oriented Programming using C++ (OOP)/Practical 4/4 MISSING NUMBER.cpp
@@ -6,41 +6,33 @@
 ////////////////////////////////////////////
 
 #include<iostream>
+
 using namespace std;
-main()
-{
-	int ARRAY[100],SIZE,LOOP,SEARCH,POSITION;
-	cout<<"Enter the Size of Array [0-99] : ";
-	cin>>SIZE;
-	cout<<endl<<endl<<endl;
-	cout<<"Enter the elements of Array:"<<endl;
-	for(LOOP=0;LOOP<SIZE;LOOP++)	
-	{
-		cin>>ARRAY[LOOP];
-	}
-	cout<<"Enter the value to search : ";
-	cin>>SEARCH;
-	for(LOOP=0;LOOP<=SIZE;LOOP++)	
-	{
-		if(SEARCH==ARRAY[LOOP])
-		{
-			POSITION=LOOP;
-			break;
-		}
-		else
-		{
-			POSITION=-999;
-		}
-	}
-	if(POSITION!=-999)
-	{
-		cout<<"The Missing Number you were searching for is at :"<<endl;
-		cout<<"Index Position : "<<POSITION<<endl;
-		cout<<"Memory Address : "<<&ARRAY[POSITION]<<endl;
-		cout<<"Element Value : "<<ARRAY[POSITION];
-	}
-	else
-	{
-		cout<<"ELEMENT NOT FOUND IN THE ARRAY !";
-	}
+main() {
+    int ARRAY[100], SIZE, LOOP, SEARCH, POSITION;
+    cout << "Enter the Size of Array [0-99] : ";
+    cin >> SIZE;
+    cout << endl << endl << endl;
+    cout << "Enter the elements of Array:" << endl;
+    for (LOOP = 0; LOOP < SIZE; LOOP++) {
+        cin >> ARRAY[LOOP];
+    }
+    cout << "Enter the value to search : ";
+    cin >> SEARCH;
+    for (LOOP = 0; LOOP <= SIZE; LOOP++) {
+        if (SEARCH == ARRAY[LOOP]) {
+            POSITION = LOOP;
+            break;
+        } else {
+            POSITION = -999;
+        }
+    }
+    if (POSITION != -999) {
+        cout << "The Missing Number you were searching for is at :" << endl;
+        cout << "Index Position : " << POSITION << endl;
+        cout << "Memory Address : " << & ARRAY[POSITION] << endl;
+        cout << "Element Value : " << ARRAY[POSITION];
+    } else {
+        cout << "ELEMENT NOT FOUND IN THE ARRAY !";
+    }
 }
