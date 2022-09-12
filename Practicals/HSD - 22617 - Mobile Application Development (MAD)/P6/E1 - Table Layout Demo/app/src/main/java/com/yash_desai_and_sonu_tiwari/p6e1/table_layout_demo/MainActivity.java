97edf2a6a579f6385f2becbda8d6e53dafa41327
Yash Desai (yd0369) <yd0369>
2022-09-12 17:06:44 +0530

    Monday, February 10, 2020 10:19:40 PM IST

---
+++ Practicals/HSD - 22617 - Mobile Application Development (MAD)/P6/E1 - Table Layout Demo/app/src/main/java/com/yash_desai_and_sonu_tiwari/p6e1/table_layout_demo/MainActivity.java
@@ -0,0 +1,16 @@
+package com.yash_desai_and_sonu_tiwari.p6e1.table_layout_demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TableRow;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
---
+++ Practicals/HSD - 22617 - Mobile Application Development (MAD)/P6/E2 - Frame Layout Demo/app/src/main/java/com/yash_desai_and_sonu_tiwari/p6e2/frame_layout_demo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.yash_desai_and_sonu_tiwari.p6e2.frame_layout_demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
