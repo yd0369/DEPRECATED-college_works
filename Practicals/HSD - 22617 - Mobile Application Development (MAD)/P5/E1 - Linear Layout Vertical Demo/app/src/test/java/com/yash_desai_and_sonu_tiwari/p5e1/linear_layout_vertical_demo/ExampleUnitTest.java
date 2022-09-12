db93fed6e1d54e62162c08911343b5b2b0c4e9ed
Yash Desai (yd0369) <yd0369>
2022-09-12 17:06:31 +0530

    Monday, February 10, 2020 10:19:40 PM IST

---
+++ Practicals/HSD - 22617 - Mobile Application Development (MAD)/P5/E1 - Linear Layout Vertical Demo/app/src/test/java/com/yash_desai_and_sonu_tiwari/p5e1/linear_layout_vertical_demo/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.yash_desai_and_sonu_tiwari.p5e1.linear_layout_vertical_demo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
---
+++ Practicals/HSD - 22617 - Mobile Application Development (MAD)/P5/E2 - Absolute Layout/app/src/main/java/com/yash_desai_and_sonu_tiwari/p5e2/absolute_layout/MainActivity.java
@@ -0,0 +1,28 @@
+package com.yash_desai_and_sonu_tiwari.p5e2.absolute_layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ((Button) findViewById(R.id.disp)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((TextView) findViewById(R.id.result)).setText(
+                        "Name : " +((TextView) findViewById(R.id.name)).getText().toString() + "\n" +
+                                "Age : " +((TextView) findViewById(R.id.age)).getText().toString() + "\n" +
+                                "Moblie Number : " +((TextView) findViewById(R.id.mobile_number)).getText().toString() + "\n"
+                );
+            }
+        });
+    }
+}
